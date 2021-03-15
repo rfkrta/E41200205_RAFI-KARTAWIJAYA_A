d4a7f43c0a9e4352c4cf7fb7027316a9bb938d26
rfkrta <80251248+rfkrta@users.noreply.github.com>
2021-03-15 17:11:28 +0700

    Hapus File

---
+++ Minggu 2/Latihan dan Tugas Wsi/src/WSI/Latihan/Latihan2.java
@@ -0,0 +1,18 @@
+package WSI.Latihan;
+
+/**
+ *
+ * @author RAFI
+ */
+public class Latihan2 {
+    public static void main(String[] args) {
+        
+        String nama = "Indonesia";
+        char abjad = 'a';
+        int a = 6;
+        double b = 6.9;
+        
+        System.out.println(nama);
+        System.out.println(a);
+    }
+}
---
+++ Minggu 2/Latihan dan Tugas Wsi/src/WSI/Tugas/person.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package WSI.Tugas;
+
+public class person {
+    public static void main(String[] args) {
+     
+        String fname, lname, stuStatus;
+        int stuId;
+        
+        fname = "Jamal";
+        lname = "Uchiha";
+        stuId = 666;
+        stuStatus = "Aktif";
+        
+        System.out.println("Student Name = " + fname +" "+ lname);
+        System.out.println("Student ID" + stuId);
+        System.out.println("Status = " + stuStatus);
+                
+        
+    } 
+}
