0e2c264bc7288b1dec297a3ab67250005d3a36ca
rfkrta <80251248+rfkrta@users.noreply.github.com>
2021-04-02 00:23:20 +0700

    Revisi

--- Minggu 4/WO4/src/Tugas/TugasB.java
+++ Minggu 4/WO4/src/Tugas/TugasB.java
@@ -4,14 +4,25 @@ package Tugas;
 public class TugasB {
     public static void main(String[] args) {
         int i = 1;
-        
+        int x =0;
         System.out.println("        Do_While");
-        System.out.println("Bilangan Kelipatan 2 (1-100)");
+        System.out.println("Bilangan Perkalian 2 (1-100)");
         System.out.println("=============================");
         do { System.out.print( i + " ");
             i=i*2;
                 }   
-       while (i<= 100); 
+       while (i<= 100);
+        
+       System.out.println("");
+       System.out.println("        Do_While");
+       System.out.println("Bilangan Kelipatan 2 (0-100)");
+       System.out.println("============================="); 
+        
+          System.out.println("");
+        do { System.out.print( x + " ");
+            x=x+2;
+                }   
+       while (x<= 100); 
           System.out.println("");
     }
     
